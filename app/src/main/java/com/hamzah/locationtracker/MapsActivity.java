642870d4d1f35b1b65709eba5157cd72245c7cdc
Mohd. Hamzah Siddiqui <36633002+mhamzahsiddiqui@users.noreply.github.com>
2018-10-04 21:00:13 +0530

    Zoom Level changed to 20.

--- app/src/main/java/com/hamzah/locationtracker/MapsActivity.java
+++ app/src/main/java/com/hamzah/locationtracker/MapsActivity.java
@@ -38,7 +38,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -51,23 +50,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             showAlert(1);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         marker = mMap.addMarker(mo);
-        /**
-         * LatLng sydney =  new LatLng(-34,151);
-         * mMap.addMarker(newMarkerOptions().positions)
-         */
     }
 
 
@@ -75,7 +61,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onLocationChanged(Location location) {
         LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
         marker.setPosition(myCoordinates);
-        int zoom = 15;
+        int zoom = 20;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myCoordinates, zoom));
     }
 
@@ -100,13 +86,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         criteria.setPowerRequirement(Criteria.POWER_HIGH);
         String provider = locationManager.getBestProvider(criteria, true);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         locationManager.requestLocationUpdates(provider, 1000, 10, this);
